cb6e9c519f21ad897d5cabcd1bfce88968410806
AdrianFoxy <drugakadrian@gmail.com>
2024-05-11 07:58:02 +0300

    encapsulation

---
+++ encapsulation/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace encapsulation
+{
+    class Gun
+    {
+        private bool isLoaded;
+
+        private void Reload()
+        {
+            Console.WriteLine("Roloading...");
+            isLoaded = true;
+            Console.WriteLine("Reloaded!");
+        }
+
+        public void Shoot()
+        {
+            if (!isLoaded) 
+            {
+                Console.WriteLine("Need reload!");
+                Reload();
+
+            } else 
+            {
+                Console.WriteLine("Shoot!");
+                isLoaded = false;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gun gun = new Gun();
+            gun.Shoot();
+        }
+    }
+}
