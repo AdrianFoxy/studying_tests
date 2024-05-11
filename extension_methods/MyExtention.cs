ff0a0118b24d9df703235a95b5ae72997da8a33d
AdrianFoxy <drugakadrian@gmail.com>
2024-05-11 09:23:02 +0300

    extension_methods

---
+++ extension_methods/MyExtention.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extension_methods
+{
+    static class MyExtention
+    {
+        public static void Print(this DateTime dateTime)
+        {
+            Console.WriteLine(dateTime);
+        }
+
+        public static bool IsDayOfWeek(this DateTime dateTime, DayOfWeek dayOfWeek) 
+        {
+            return dateTime.DayOfWeek == dayOfWeek;
+        }
+
+        public static string GetFullName(this Student student)
+        {
+            return student.LastName + " " + student.FirstName;
+        }
+    }
+}
---
+++ extension_methods/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace extension_methods 
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Extentions works only in the same namespace
+            DateTime currentDateTime = DateTime.Now;
+            currentDateTime.Print();
+
+            DateTime.Now.Print();
+
+            Console.WriteLine(currentDateTime.IsDayOfWeek(DayOfWeek.Saturday));
+
+            Student student = new Student() { FirstName = "Bob", LastName = "Leboske" };
+            string fullName = student.GetFullName();
+            Console.WriteLine(fullName);
+        }
+    }
+}
