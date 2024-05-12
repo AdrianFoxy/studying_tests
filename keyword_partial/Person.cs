359087591c94a34b266369b7a2f5a1bdb776d204
AdrianFoxy <drugakadrian@gmail.com>
2024-05-12 07:22:48 +0300

    keyword_partial

---
+++ keyword_partial/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keyword_partial
+{
+    partial class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firtsName, string lastName)
+        {
+            FirstName = firtsName;
+            LastName = lastName;
+        }
+
+        public partial string GetFullName();
+    }
+}
---
+++ keyword_partial/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace keyword_partial
+{
+    class Program
+    {
+        // Partial classes can be razneseni to different files
+        static void Main(string[] args)
+        {
+            Person person = new Person("Bob", "Mils");
+            person.PrintFullName();
+        }
+    }
+}
