03d5fa7f7516005d6d6cd92e5569f2d430fc7d9f
AdrianFoxy <drugakadrian@gmail.com>
2024-06-08 08:49:58 +0300

    events

---
+++ events/Hero.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace events
+{
+    public class Hero
+    {
+        private int health;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public delegate void HealthChangedHandler(object sender, int health);
+
+        public event HealthChangedHandler OnHealthChanged;
+
+        public int Health
+        {
+            get => this.health;
+            set
+            {
+                if (this.health != value)
+                {
+                    this.health = value;
+                    OnHealthChanged?.Invoke(this, this.health);
+                }
+            }
+        }
+    }
+
+}
---
+++ events/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hero = new Hero()
+            {
+                Id = 1,
+                Name = "Bob",
+                Health = 100
+            };
+
+            hero.OnHealthChanged += HeroOnHealthChanged;
+            hero.OnHealthChanged += HeroIsDead;
+
+            for (int i = 0; i < 5; i++)
+            {
+                hero.Health -= 20;
+            }
+        }
+
+        private static void HeroIsDead(object sender, int health)
+        {
+            var hero = (Hero)sender;
+            if (hero.Health <= 0)
+            {
+                Console.WriteLine($"{hero.Name} is dead");
+            }
+        }
+
+        private static void HeroOnHealthChanged(object sender, int health)
+        {
+            var hero = (Hero)sender;
+            Console.WriteLine($"{hero.Name} has new health: {health}");
+        }
+    }
+}
