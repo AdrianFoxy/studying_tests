c92d50f2e892ce1f92c3a641325cf7bb7df6059a
AdrianFoxy <drugakadrian@gmail.com>
2024-06-08 11:34:51 +0300

    test for async/await

---
+++ async_await/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace async_await
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            SummonDogLocally();
+        }
+
+        static async Task SummonDogLocally()
+        {
+            Console.WriteLine("1. Summoning Dog Locally...");
+
+            string dogText = await File.ReadAllTextAsync("dog.txt");
+
+            Console.WriteLine($"2. Dog Summoned Localy \n{dogText}");
+        }
+    }
+}
