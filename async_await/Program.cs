33ebf888e995d539a239f21b0be0a51b959fc6cf
AdrianFoxy <drugakadrian@gmail.com>
2024-06-08 15:06:44 +0300

    multithreding+async/await

--- async_await/Program.cs
+++ async_await/Program.cs
@@ -1,19 +1,43 @@
-﻿namespace async_await
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace async_await
 {
     class Program
     {
-        static void Main(string[] args) 
+        static async Task Main(string[] args)
         {
-            SummonDogLocally();
+            /*await SummonDog1();
+            await SummonDog2();*/
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            var tasks = new List<Task> { SummonDog1(), SummonDog2() };
+            await Task.WhenAll(tasks);
+
+            sw.Stop();
+            Console.WriteLine("We are done here " + sw.Elapsed.TotalSeconds);
         }
 
-        static async Task SummonDogLocally()
+        static async Task SummonDog1()
         {
-            Console.WriteLine("1. Summoning Dog Locally...");
-
+            Console.WriteLine("1. Summoning Dog1...");
             string dogText = await File.ReadAllTextAsync("dog.txt");
 
-            Console.WriteLine($"2. Dog Summoned Localy \n{dogText}");
+            await Task.Delay(1000);
+
+            Console.WriteLine($"Here is our doggo number 1: \n" + dogText);
+        }
+
+        static async Task SummonDog2()
+        {
+            Console.WriteLine("2. Summoning Dog1...");
+            string dogText = await File.ReadAllTextAsync("dog2.txt");
+
+            Console.WriteLine($"Here is our doggo number 2: \n" + dogText);
         }
     }
 }
---
+++ multithreading_help/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace multithreading_help
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task1 = Task.Run(() => DoWork("Task 1"));
+            Task task2 = Task.Run(() => DoWork("Task 2"));
+
+            Console.WriteLine("Main method completed.");
+
+            Task.WaitAll(task1, task2);
+
+            Console.WriteLine("Both tasks completed.");
+
+        }
+
+        static void DoWork(string taskName)
+        {
+            Console.WriteLine($"{taskName} started.");
+            Task.Delay(2000).Wait();
+            Console.WriteLine($"{taskName} completed.");
+        }
+
+    }
+}
