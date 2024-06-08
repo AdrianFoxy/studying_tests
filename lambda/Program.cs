e22456a7ac604c55ea562df75b07098a9c954d28
AdrianFoxy <drugakadrian@gmail.com>
2024-06-08 16:44:22 +0300

    lambda

---
+++ lambda/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace lambda
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>()
+            {
+                new Person { Name = "John", Age = 25},
+                new Person { Name = "Potato man", Age = 32},
+                new Person { Name = "Bruh mem", Age = 19}
+            };
+        }
+    }
+}
