8609539085b8ae96d89236034c298695e59e1773
AdrianFoxy <drugakadrian@gmail.com>
2024-06-09 09:48:21 +0300

    LINQ

---
+++ dispose_finalize/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace dispose_finalize
+{
+    class Person
+    {
+        public string Name { get; }
+        public Person(string name) => Name = name;
+
+        ~Person()
+        {
+            Console.WriteLine($"{Name} has deleted");
+        }
+    }
+
+    public class Person2 : IDisposable
+    {
+        public string Name { get; }
+        public Person2(string name) => Name = name;
+
+        public void Dispose()
+        {
+            Console.WriteLine($"{Name} has been disposed");
+        }
+    }
+
+    public class Person3 : IDisposable
+    {
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    class Program
+    {
+        static void Test2()
+        {
+            Person2? tom = null;
+            try
+            {
+                tom = new Person2("Tom");
+            }
+            finally
+            {
+                tom?.Dispose();
+            }
+        }
+
+        static void Test()
+        {
+            Person tom = new Person("Tom");
+        }
+
+        static void Main(string[] args)
+        {
+            Test();
+            GC.Collect();
+            Console.Read();
+
+            Test2();
+        }
+    }
+}
+
---
+++ what_is_LINQ/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace what_is_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            // Standart
+            int[] numbers = { 5, 10, 8, 3, 6, 12 };
+
+            var evenNumbers = from num in numbers
+                              where num % 2 == 0
+                              select num;
+
+            foreach (var n in evenNumbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Extention
+            List<string> fruits = new List<string> { "Apple", "Banana", "Mango", "Orange", "Pineapple" };
+            var fruitsWithA = fruits.Where(fruit => fruit.Contains("a")).OrderBy(fruit => fruit);
+
+            Console.WriteLine("\nФрукты, содержащие букву 'a':");
+            foreach (var fruit in fruitsWithA)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
